959b45908ba308220857d570a11f3fd9603660ff
Tyler Holhubner <tholhubner@gmail.com>
2023-06-29 22:58:42 -0400

    Car model complete

--- server/models/Car.js
+++ server/models/Car.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema
 const Car = new Schema(
 	{
 		name: { type: String, required: true },
+		horsepower: { type: Number, required: true },
+		torque: { type: Number, required: true },
+		class: { type: String, required: true },
 	},
 	{ timeseries: true }
 )
