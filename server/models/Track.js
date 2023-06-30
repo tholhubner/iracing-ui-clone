bb6733036b2e5f7906b37d00a63355973aebca41
Tyler Holhubner <tholhubner@gmail.com>
2023-06-29 23:05:40 -0400

    Track model complete

--- server/models/Track.js
+++ server/models/Track.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema
 const Track = new Schema(
 	{
 		name: { type: String, required: true },
+		description: { type: Blob, required: true },
+		location: { type: String, required: true },
+		configLength: { type: Number, required: true },
+		nightLights: { type: Boolean, required: true },
+		configsAvailable: {type: Number, required: true },
 	},
 	{ timeseries: true }
 )
