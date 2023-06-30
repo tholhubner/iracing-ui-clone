f93d00f9965cc8d1fe257430ae7f52cf3e43bf73
Tyler Holhubner <tholhubner@gmail.com>
2023-06-29 22:59:18 -0400

    added missing detail to car model

--- server/models/Car.js
+++ server/models/Car.js
@@ -4,9 +4,24 @@ const Schema = mongoose.Schema
 const Car = new Schema(
 	{
 		name: { type: String, required: true },
+		description: { type: Blob, required: true },
 		horsepower: { type: Number, required: true },
 		torque: { type: Number, required: true },
 		class: { type: String, required: true },
+		carLength: { type: Number, required: true },
+		carWidth: { type: Number, required: true },
+		carHeight: { type: Number, required: true },
+		wheelbase: { type: Number, required: true },
+		carWeight: { type: Number, required: true },
+		engine: { type: String, required: true },
+		fuelCapacity: { type: Number, required: true },
+		gearbox: { type: String, required: true },
+		aerodynamics: { type: String, required: true },
+		suspension: { type: String, required: true },
+		lubrication: { type: String, required: true },
+		cooling: { type: String, required: true },
+		brakes: { type: String, required: true },
+		wheels: { type: String, required: true }
 	},
 	{ timeseries: true }
 )
