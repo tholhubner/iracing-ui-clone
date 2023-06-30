d8cd9db5e673ea20e8000aece0f19be4775e31f8
Tyler Holhubner <tholhubner@gmail.com>
2023-06-29 23:17:09 -0400

    Series model complete

--- server/models/Series.js
+++ server/models/Series.js
@@ -4,6 +4,14 @@ const Schema = mongoose.Schema
 const Series = new Schema(
 	{
 		title: { type: String, required: true },
+		information: { type: String, required: true },
+		car: { type: String, required: true },
+		seriesType: { type: String, required: true },
+		maxLicense: { type: String, required: true },
+		minLicense: { type: String, required: true },
+		fixedSeries: { type: Boolean, required: true },
+		minDrivers: { type: Number, required: true },
+		maxDrivers: { type: Number, required: true },
 	},
 	{ timeseries: true }
 )
