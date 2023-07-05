a402603da80c0af9b0513de839f06abca3d05542
Tyler Holhubner <tholhubner@gmail.com>
2023-07-04 22:59:13 -0400

    created some basic test data for a series list

---
+++ client/src/testData/series.js
@@ -0,0 +1,26 @@
+export const seriesList = [
+	{
+		title: "Testing",
+		license: "C",
+		car: ["Toyota GR86 Cup"],
+		track: "Okayama Circuit",
+		fixed: true,
+		teamEvent: false,
+	},
+	{
+		title: "Testing 2",
+		license: "B",
+		car: ["Audi R8 GT3", "BMW M4 GT3"],
+		track: "Suzuka Circuit (GP)",
+		fixed: false,
+		teamEvent: true,
+	},
+	{
+		title: "Testing 3",
+		license: "A",
+		car: ["Mercedes W13"],
+		track: "Summit Point Raceway",
+		fixed: false,
+		teamEvent: false,
+	}
+]
