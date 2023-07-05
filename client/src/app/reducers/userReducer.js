cbdf74b158bd7e00b862c7f963a69604f4599d91
Tyler Holhubner <tholhubner@gmail.com>
2023-07-04 23:25:58 -0400

    created store for user data, created basic user test data and added reducers for series favorites on users

---
+++ client/src/app/reducers/userReducer.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { userData } from "../../testData/user"
+
+const initialState = userData
+
+export const variousReducer = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addSeriesToFavorites: (state, action) => {
+			const tempArray = state.favoriteTracks
+			tempArray.push(action.payload)
+      state.favoriteTracks = tempArray
+    },
+		removeSeriesFromFavorites: (state, action) => {
+			const index = state.favoriteTracks.indexOf(action.payload)
+			const tempArray = state.favoriteTracks
+			if (index > -1) {
+				tempArray.splice(index, 1)
+			}
+			state.favoriteTracks = tempArray
+		}
+  },
+})
+
+export const { addSeriesToFavorites, removeSeriesFromFavorites } = userReducer.actions
+
+export default userReducer.reducer
---
+++ client/src/testData/user.js
@@ -0,0 +1,35 @@
+export const userData = {
+	licenses: [
+		{
+			type: "road",
+			class: "A",
+			safetyRating: 2.34,
+			iRating: 1234,
+			ttRating: 1333,
+		},
+		{
+			type: "oval",
+			class: "C",
+			safetyRating: 2.34,
+			iRating: 1234,
+			ttRating: 1333,
+		},
+		{
+			type: "dirtRoad",
+			class: "B",
+			safetyRating: 2.34,
+			iRating: 1234,
+			ttRating: 1333,
+		},
+		{
+			type: "dirtOval",
+			class: "rookie",
+			safetyRating: 2.34,
+			iRating: 1234,
+			ttRating: 1333,
+		},
+	],
+	favoriteSeries: [],
+	favoriteCars: [],
+	favoriteTracks: [],
+}
